961fee0215ca31ed931df5d1908e4c311d4c84fe
Grzegorz Miklaszewski <mail@gmiklaszewski.pl>
2011-07-14 03:17:34 +0200

    Fixed hook generation

--- lib/gino/client/generators/git_hook.rb
+++ lib/gino/client/generators/git_hook.rb
@@ -8,10 +8,14 @@ module Gino
       module GitHook
         def self.generate(repository_name, repository_path, api_url)
           filepath = File.join(repository_path, "hooks/post-receive")
-          template = File.expand_path(File.read(File.dirname(__FILE__) + '/git_hook.erb'))
-          File.open(filepath, 'w') do |f| 
-            f.write ERB.new(template).result binding
-          end
+          template = File.read(File.expand_path(File.dirname(__FILE__) + '/git_hook.erb'))
+          
+          api_uri = URI.parse(api_url)
+          api_uri.path = '/' if api_uri.path.empty?
+          
+          f= File.new(filepath, 'w', 0755)
+          f.write ERB.new(template).result binding
+          f.close
         end
       end
     end
